9e20b3bb602fc396e3daf03d47f6320f58383843
ivanpazbek <ivan.pazbek@gmail.com>
2023-01-19 00:18:23 +0000

    Update 3 files

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,7 +9,7 @@
       <span
         id="nav-title"
         class="leading-none self-center text-2xl sm:text-4xl font-semibold whitespace-nowrap text-white md:text-5xl mt-1 mb-2"
-        >Julian Passebecq // Data Portfolio</span
+        >Julian Passebecq</span
       >
       <div class="w-auto text-white py-2.5" id="navbar-buttons">
         <button
