7d600616553ff5b44da1cb56d3195a696502dbf7
ivanpazbek <ivan.pazbek@gmail.com>
2023-01-26 22:27:55 +0000

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,7 +10,7 @@
         id="nav-title"
         class="leading-none self-center text-2xl sm:text-4xl font-semibold whitespace-nowrap text-white md:text-5xl mt-1 mb-2"
         >Julian PASSEBECQ - 27 y.o</span>
-        <h1 class="title4"><br/>English C1<br/>German B1 <br/>French Native </h1>
+        <h1 class="title4">English C1, German B1, French Native </h1>
       
       <div class="w-auto text-white py-2.5" id="navbar-buttons">
         <button
@@ -66,6 +66,8 @@
 .title4 {
   font-size: 10px;
   position: relative;
+  left:-200px;
+  bottom: -25px;
   color:white;
   font-family: "Anvir", serif;
   margin-top: -10px;
