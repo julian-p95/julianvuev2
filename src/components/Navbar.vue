bb19dd6c3a53af8f56096a2d448d7d27731770b8
ivanpazbek <ivan.pazbek@gmail.com>
2023-01-29 23:02:48 +0000

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,7 +9,7 @@
       <span
         id="nav-title"
         class="leading-none self-center text-2xl sm:text-4xl font-semibold whitespace-nowrap text-white md:text-5xl mt-1 mb-2"
-        ><u>Julian PASSEBECQ</u><br>  Energy Engineer<br>Data Specialization</span>
+        ><u>Julian PASSEBECQ</u><br>  Energy Engineer<br>4 years Data Exp</span>
        
       
       <div class="w-auto text-white py-2.5" id="navbar-buttons">
@@ -23,7 +23,7 @@
         <path d="M3 24h19v-23h-1v22h-18v1zm17-24h-18v22h18v-22zm-3 17h-12v1h12v-1zm0-3h-12v1h12v-1zm0-3h-12v1h12v-1zm-7.348-3.863l.948.3c-.145.529-.387.922-.725 1.178-.338.257-.767.385-1.287.385-.643 0-1.171-.22-1.585-.659-.414-.439-.621-1.04-.621-1.802 0-.806.208-1.432.624-1.878.416-.446.963-.669 1.642-.669.592 0 1.073.175 1.443.525.221.207.386.505.496.892l-.968.231c-.057-.251-.177-.449-.358-.594-.182-.146-.403-.218-.663-.218-.359 0-.65.129-.874.386-.223.258-.335.675-.335 1.252 0 .613.11 1.049.331 1.308.22.26.506.39.858.39.26 0 .484-.082.671-.248.187-.165.322-.425.403-.779zm3.023 1.78l-1.731-4.842h1.06l1.226 3.584 1.186-3.584h1.037l-1.734 4.842h-1.044z"/>
       </svg>
           
-          <span class="mt-1">4 yr Exp</span>
+          <span class="mt-1">CV</span>
         </button>
       </div>
 
@@ -89,11 +89,11 @@ graph_one {
   background-image: linear-gradient(
     90deg,
     #000000,
-    #6c6969,
-    #90aaff,
-    #9c62ff,
+    #000000,
+    #000001,
+    #000000,
     #0000009b,
-    #abafa9
+    #020500
   );
 
 
@@ -112,39 +112,39 @@ graph_one {
 .graph_one {
   margin-top: 0px;
   margin-bottom: -5px;
-  margin-left:-25px;
+  margin-left:0px;
   margin-right:-13px;
 }
 .graph_one th {
-  color: #fff;
+  color: #000000;
   margin-top: 0px;
   font-style: italic;
   font-weight: 200;
-  font-size: 6px;
+  font-size: 10px;
 }
 .graph_one tr {
   display: flex !important;
   flex-direction: column-reverse !important;
-  width: 18px;
-  margin: 4px;
+  width: 25px;
+  margin: 5px;
 }
 .graph_one td {
   border: 1px solid #fff;
 }
 .graph_one .td_one {
-  height: 35px;
+  height: 55px;
 }
 .graph_one .td_two {
-  height: 20px;
+  height: 40px;
 }
 .graph_one .td_three {
-  height: 15px;
+  height: 35px;
 }
 .graph_one .td_four {
-  height: 30px;
+  height: 50px;
 }
 .graph_one .td_five {
-  height: 15px;
+  height: 35px;
 }
 .graph_one tbody {
   display: flex !important;
@@ -168,7 +168,7 @@ graph_one {
   
 }
 #navbar {
-  background-color: #87ccfacd;
+  background-color: #ffffffcd;
   
 }
 .nav-button:hover .nav-icon {
